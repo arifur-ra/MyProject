b6b0fc08a6e41c57b3915e3fd5ae6a5f208e8814
Arifur Rahman Arif <ararif@ararif.localdomain>
2021-08-06 11:14:15 +0200

    ecommerce shop

---
+++ ecommerce-store/README.md
@@ -0,0 +1,7 @@
+# React Simple E commerce Shop Application
+
+This project is about the e-commerce site where store the product.
+
+## Technology used:
+
+### XML CSS React Redux RestApI,
---
+++ ecommerce-store/src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import Header from "./container/Header";
+import ProductDetails from "./container/ProductDetails";
+import ProductListing from "./container/ProductListing";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={ProductListing} />
+          <Route path="/product/:productId" component={ProductDetails} />
+          <Route>404 Not Found!</Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
