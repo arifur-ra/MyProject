777c4b1e6897941663f96f506409b2a9fce194ab
Arifur Rahman Arif <ararif@ararif.localdomain>
2021-08-15 11:17:57 +0200

    adding new project

--- 14_countdown-timer/app.js
+++ 14_countdown-timer/app.js
@@ -29,14 +29,13 @@ const items =document.querySelectorAll('.deadline-format h4')
 let tempDate = new Date()
 let tempYear = tempDate.getFullYear()
 let tempMonth = tempDate.getMonth()
-let tempDay = tempDate.getDay()
+let tempDay = tempDate.getDate()
 
 
 
-  let futureDate = new Date(2021,11,31,16,30,0)
-// console.log(futureDate)
+// let futureDate = new Date(2021,11,31,16,30,0)
 
-// let  futureDate = new Date(tempYear, tempMonth, tempDay + 10, 16,30,0);
+let  futureDate = new Date(tempYear, tempMonth, tempDay + 10, 4,30,0);
 
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours();    
@@ -48,7 +47,7 @@ const date =futureDate.getDate()
 
 const weekday = weekdays[futureDate.getDay()]
 
-endCourse.innerHTML=` on ${weekday}, ${date} ${month} ${year} || ${hours}:${mins}`
+endCourse.innerHTML=` on ${weekday}, ${date} ${month} ${year} || ${hours}:${mins}pm`
 
 
 
